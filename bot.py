70cd06b0f3959c36fafd448e1dccf5c77e3980da
yeb1ch <89690018+yeb1ch@users.noreply.github.com>
2022-11-09 13:53:38 +0500

    proper weather info

--- bot.py
+++ bot.py
@@ -29,8 +29,9 @@ def on_message(message):
     else:
         if menu_handler.current_menu == 'enter_city_name':
             image_link = get_weather_picture(message.text.lower())
+            city_name = message.text.lower()
             bot.send_photo(message.chat.id, photo=open(r'img\autumn\AutumnClouds.png', 'rb'),
-                           caption=get_current_weather_info('екатеринбург'), reply_markup=menu_handler.markup)
+                           caption=get_current_weather_info(message.text.lower()), reply_markup=menu_handler.markup)
             menu_handler.change_menu('weather_info')
     bot.send_message(message.from_user.id, f'{menu_handler.current_menu}', reply_markup=menu_handler.markup)
 
